c17cc5afd9fa5b494e9504adae62581efccf0ba7
shahinexy <shahinexy@gmail.com>
2024-01-30 00:22:14 +0600

    task 2 added

--- js task/task.js
+++ js task/task.js
@@ -18,3 +18,28 @@ var haveMoney = 1000;
 var costMoney = 700;
 var result = haveMoney - costMoney;
 console.log(result);
+
+
+// Task-2
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// Input:
+// The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+// Output:
+// Print the result in 2 decimal places.
+
+// Sample Input:
+// 75.25, 65, 80, 35.45, 99.50
+
+// Sample Output:
+// 71.04
+
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var sum = math + biology + chemistry + physics + bangla;
+var sumResult = sum / 5;
+console.log(sumResult);
