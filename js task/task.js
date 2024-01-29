d208cd889aae4efb31f1daeb20450b645b1daefa
shahinexy <shahinexy@gmail.com>
2024-01-30 00:37:36 +0600

    task 3 added

--- js task/task.js
+++ js task/task.js
@@ -42,4 +42,27 @@ var physics = 35.45;
 var bangla = 99.50;
 var sum = math + biology + chemistry + physics + bangla;
 var sumResult = sum / 5;
-console.log(sumResult);
+console.log(sumResult.toFixed(2));
+
+// Task-3
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 4
+
+var number = 119;
+console.log(number % 5);
+
+// var a = isNaN('1');
+var a = isNaN(2-10);
+console.log(a);
+
