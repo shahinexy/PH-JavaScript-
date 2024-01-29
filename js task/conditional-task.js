6a6bb8d465e4768ec0fbbbf43c314a9ad10c54e1
shahinexy <shahinexy@gmail.com>
2024-01-30 00:43:38 +0600

    conditional task file added

---
+++ js task/conditional-task.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 400;
+if(burgerPrice >= 500){
+    console.log('you got a free coke');
+}
+else{
+    console.log('You have to pay 30tk for coke');
+}
