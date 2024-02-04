0071111d52585d8e8b97dc94593fb0c66968248a
shahinexy <shahinexy@gmail.com>
2024-02-04 23:31:51 +0600

    work on string js

--- string.js
+++ string.js
@@ -9,3 +9,46 @@ console.log(upperCase);
 
 const lowerCase = str.toLowerCase();
 console.log(lowerCase);
+
+console.log('================');
+
+// slice 
+const sliceStr = 'narayanganj';
+const slice = sliceStr.slice(4,8);
+console.log(slice);
+
+console.log('===================');
+
+// split 
+const splitStr = 'I am a font-end web developer';
+const split = splitStr.split(' ');
+const splitx = splitStr.split('a');
+console.log(split);
+console.log('=========');
+console.log(splitx);
+console.log('=============');
+const splitStr2 = 'shahin|rifat|shifat|kholil'
+const split2 = splitStr2.split('|')
+console.log(split2);
+
+// Join 
+const joinArray = [ 'shahin', 'rifat', 'shifat', 'kholil' ]
+console.log(joinArray.join());
+console.log('===============');
+console.log(joinArray.join(' | '));
+console.log('============================');
+
+
+
+// =================================
+//         Concat in String
+// =================================
+const firstName = 'Shahin';
+const  lastName = 'Alam';
+console.log(firstName + ' ' + lastName);
+console.log(firstName.concat(' ', lastName));
+console.log('================');
+
+// Include 
+console.log(firstName.includes('a'));
+console.log(firstName.includes('y'));
