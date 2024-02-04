66947a7612eb9ac05dc451f159e8a320bb703e38
shahinexy <shahinexy@gmail.com>
2024-02-04 23:51:34 +0600

    work on string js

--- string.js
+++ string.js
@@ -52,3 +52,27 @@ console.log('================');
 // Include 
 console.log(firstName.includes('a'));
 console.log(firstName.includes('y'));
+
+console.log( '====================');
+// reverce centence 
+const sentence = 'I am learning web development.';
+let revers = '';
+for(let letter of sentence){
+    revers = letter + revers;
+}
+console.log(revers);
+
+console.log( '==================');
+
+let rev = '';
+for(let i =0; i<sentence.length; i++){
+    const letter = sentence[i];
+    rev = letter + rev;
+}
+console.log(rev);
+
+console.log('===============');
+
+// Shortcute 
+const stnReverse = sentence.split('').reverse().join('');
+console.log(stnReverse);
