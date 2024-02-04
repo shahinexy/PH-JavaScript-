7256926cef2b13176f1f42d3bf7aee10706052d1
shahinexy <shahinexy@gmail.com>
2024-02-05 00:55:54 +0600

    wrok on object file

--- object.js
+++ object.js
@@ -7,7 +7,8 @@ const person = {
         Brother : 1,
         Sister : 3,
     },
-    'present address' : 'narayanganj'
+    'present address' : 'narayanganj',
+    id : 23112,
 }
 console.log(person);
 console.log(person.Name);
@@ -23,3 +24,35 @@ console.log('============');
 person['isStudent'] = false;
 console.log(person['isStudent']);
 console.log(person);
+
+console.log('=====================');
+
+// use object key to get objest keys 
+console.log(Object.keys(person));
+
+console.log('====================');
+
+// use object value to get object values 
+console.log(Object.values(person));
+
+console.log('================');
+
+// to delete a property from an object use delete 
+delete person.id;
+console.log(person);
+
+console.log('===============');
+
+// ===================
+//     looping in Object
+// =====================
+
+for(let key in person){
+    console.log(key);
+}
+
+console.log('=======xx==========');
+
+for(let key in person){
+    console.log(person[key]);
+}
