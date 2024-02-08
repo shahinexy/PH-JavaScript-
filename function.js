03d207f203e1bada441491ccd819d4d5fbb31f35
shahinexy <shahinexy@gmail.com>
2024-02-08 22:30:37 +0600

    something add on function arguments

--- function.js
+++ function.js
@@ -5,3 +5,13 @@ function myFunction(x,y){
 console.log(myFunction(10,20));
 console.log(myFunction(13,40));
 console.log(myFunction(354,453));
+
+console.log('========================');
+
+// Check it out difrent somthing.(arguments)
+function add( x, y){
+    console.log(x,y);
+    console.log(arguments);
+    console.log(arguments[2]);
+}
+add(10,20,45,60,23);
