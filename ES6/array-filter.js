ee41becbd69e6dbb49701dcd3db303f37e65ab8f
shahinexy <shahinexy@gmail.com>
2024-02-23 16:32:34 +0600

    array filter added

---
+++ ES6/array-filter.js
@@ -0,0 +1,11 @@
+// ============= By Rahat vai ================
+// forEach return a new array
+// return multiple element from an array based on condition or test case
+
+const filterdNumber = [3,46,87,5,4,98,56]
+const filterResult = filterdNumber.filter((number) => number > 46)
+console.log(filterResult);
+// =========================================================
+
+const oddNumber = filterdNumber.filter(number => number % 2 !== 0);
+console.log(oddNumber);
