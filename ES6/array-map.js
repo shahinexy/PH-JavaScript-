faad0a37e68f6586a117998c8409de11a19a6f6f
shahinexy <shahinexy@gmail.com>
2024-02-23 16:20:37 +0600

    array map added

---
+++ ES6/array-map.js
@@ -0,0 +1,20 @@
+// ================================ by rahat vai ===================
+// map can return and console.log 
+const myarray = [23,43,56,12,65,67];
+const result = myarray.map((number,index,array) => {
+    return (number * 2);
+})
+// console.log(result);
+console.log('============');
+
+const myarray2 = [23,43,56,12,65,67];
+const result2 = myarray2.map((number,index,array) => {
+    return `number ${number}, index ${index}, array ${array} `;
+})
+console.log(result2);
+console.log('=============');
+// ======================================================================
+
+const arr = [2,6,45,87,44];
+const arrresult = arr.map(numb => numb / 2);
+console.log(arrresult);
