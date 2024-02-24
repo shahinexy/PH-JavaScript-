15eb0c12774c4dab1cd18bc1f07a740ba5d03c55
shahinexy <shahinexy@gmail.com>
2024-02-24 11:13:49 +0600

    array find added

--- ES6/Array Iteration/array-filter.js
+++ ES6/Array Iteration/array-filter.js
@@ -2,10 +2,14 @@
 // forEach return a new array
 // return multiple element from an array based on condition or test case
 
-const filterdNumber = [3,46,87,5,4,98,56]
+const filterdNumber = [3, 46, 87, 5, 4, 98, 56]
 const filterResult = filterdNumber.filter((number) => number > 46)
 console.log(filterResult);
 // =========================================================
 
 const oddNumber = filterdNumber.filter(number => number % 2 !== 0);
 console.log(oddNumber);
+
+const friends = ['rifat', 'rafi', 'shifat', 'sujon', 'kholil', 'nazmul', 'suvo']
+const friendName = friends.filter(friend => friend.length > 5);
+console.log(friendName);
---
+++ ES6/Array Iteration/array-find.js
@@ -0,0 +1,7 @@
+// ============= by Rahat vai ============
+// break when match the condision. only return 1 element 
+
+const findNumber = [3,46,87,5,4,98,56]
+const findResult = findNumber.find((number) => number > 46)
+console.log(findResult);
+// ==========================================
