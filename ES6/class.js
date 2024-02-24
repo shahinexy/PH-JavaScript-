3b9fe380adc1f64c1fd0d407c441fe47b941e75c
shahinexy <shahinexy@gmail.com>
2024-02-24 23:15:47 +0600

    class

---
+++ ES6/class.js
@@ -0,0 +1,14 @@
+class teacher{
+    constructor(name, subject){
+        this.name = name;
+        this.subject = subject;
+    }
+}
+const siplu = new teacher('Siplu', 'Math')
+console.log(siplu);
+
+const sumon = new teacher('Sumon', 'Chemistry')
+console.log(sumon);
+
+const rigan = new teacher('Rigan', 'English')
+console.log(rigan);
