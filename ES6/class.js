24d32d6412f23dbed5fcf450cae90c38ae557c53
shahinexy <shahinexy@gmail.com>
2024-02-25 11:51:55 +0600

    inheritance

--- ES6/class.js
+++ ES6/class.js
@@ -12,3 +12,31 @@ console.log(sumon);
 
 const rigan = new teacher('Rigan', 'English')
 console.log(rigan);
+
+// ===================================================
+//                 Inheritance
+// ===================================================
+class vehicle{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+    move(){
+        console.log('gari cole');
+    }
+}
+
+class bus extends vehicle{
+    constructor(name, price, seat ,ticketPrice){
+        super(name, price);
+        this.seat = seat;
+        this.ticketPrice = ticketPrice;
+    }
+}
+
+class track extends vehicle{
+    constructor(name, price, load){
+        super(name, price);
+        this.load = load;        
+    }
+}
