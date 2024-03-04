3d17eae34c618a7b8301346427ea855b91a89704
shahinexy <shahinexy@gmail.com>
2024-03-04 23:28:49 +0600

    setTimeout

---
+++ ES6/setTimeOut.js
@@ -0,0 +1,13 @@
+console.log(1);
+console.log(2);
+// set time out will execute after all execution.
+setTimeout(()=>{
+   console.log(3); 
+})
+// it will execute after 3s. we can set delay time by this.
+setTimeout(()=>{
+    console.log(4); 
+}, 3000)
+console.log(5);
+console.log(6);
+console.log(7);
