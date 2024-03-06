91172147b3f4ad010e19ab2e5c27ec8001403f83
shahinexy <shahinexy@gmail.com>
2024-03-06 14:37:40 +0600

    error fild added

---
+++ more js/try-catch-throw-finally.js
@@ -0,0 +1,6 @@
+function submit(){
+    const inputFild = document.getElementById('input_fild');
+    const errorFild = document.getElementById('error_fild').innerText;
+    const inputValue = inputFild.value;
+    console.log(inputValue);
+}
